636417c0ebd4cccbc44a5fdf43db2423598f373d
Zk <zklim95@hotmail.com>
2024-04-14 03:27:17 +0800

    in_progress: deposit

--- src/Pool.sol
+++ src/Pool.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.25;
 
 import {Owned} from "solmate/auth/Owned.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 
 contract Pool is Owned {
 	enum POOLSTATUS {
@@ -9,6 +11,12 @@ contract Pool is Owned {
 		DEPOSIT_ENABLED,
 		DEPOSIT_DISABLED,
 		FUND_DISTRIBUTED
+	}
+
+	struct Participant {
+		address participant;
+		uint256 deposit;
+		uint256 feesCharged;
 	}
 
 	struct PoolDetails {
@@ -19,26 +27,40 @@ contract Pool is Owned {
 		uint40 timeStart;
 		uint40 timeEnd;
 
+		// Pool fees
+		uint16 penaltyFeesRate;	// 0.01% to 100% (10000)
+
+		// Pool capacity
+		uint32 maxParticipants;
+		uint32 numParticipantsJoined;
+
 		// Pool addresses
 		address host;
 		address cohost;
-		address token;
+		IERC20 token;
 
 		// Pool details
 		string poolName;
+		uint256 amountToDeposit;
 
 		// Pool balances
 		uint256 totalDeposits;
+		uint256 feesCollected;
 		uint256 balance;
+
+		Participant[] participants;
 	}
+	
+	uint16 constant public FEES_PRECISION = 10000;
 
 	uint256 public latestPoolId;
 
 	mapping (uint256 => PoolDetails) public pools;
 	mapping (address => uint256[]) public createdPools;
 
-	event PoolCreated(uint256 poolId, address host, address token);
+	event PoolCreated(uint256 poolId, address host, PoolDetails poolDetails);
 	event PoolStatusChanged(uint256 poolId, POOLSTATUS status);
+	event Deposit(uint256 poolId, address participant, uint256 amount);
 
 	constructor(address _owner) Owned(_owner) {}
 
@@ -48,33 +70,43 @@ contract Pool is Owned {
 	/// @param cohost The cohost address
 	/// @param timeStart The start time of the pool
 	/// @param timeEnd The end time of the pool
+	/// @param penaltyFeesRate The penalty fees rate
 	/// @return poolId The pool id
 	/// @dev Pool status will be INACTIVE
 	/// @dev Pool will be added to createdPools mapping
 	/// @dev Emits PoolCreated event
 	function createPool(
 		string calldata poolName,
-		address token,
+		IERC20 token,
 		address cohost,
 		uint40 timeStart,
-		uint40 timeEnd
+		uint40 timeEnd,
+		uint16 penaltyFeesRate,
+		uint32 maxParticipants,
+		uint256 amountToDeposit
 	) external returns (uint256 poolId) {
 		require(timeStart < timeEnd, "Invalid timing");
-		require(token != address(0), "Invalid token");
+		require(address(token) != address(0), "Invalid token");
+		require(penaltyFeesRate <= FEES_PRECISION, "Invalid fees rate");
+		require(amountToDeposit > 0, "Invalid deposit amount");
 		poolId = latestPoolId;
 
-		pools[poolId] = PoolDetails({
-			status: POOLSTATUS.INACTIVE,
-			timeStart: timeStart,
-			timeEnd: timeEnd,
-			host: msg.sender,
-			cohost: cohost,
-			token: token,
-			poolName: poolName,
-			totalDeposits: 0,
-			balance: 0
-		});
-		emit PoolCreated(poolId, msg.sender, token);
+		pools[poolId].timeStart = timeStart;
+		pools[poolId].timeEnd = timeEnd;
+		pools[poolId].penaltyFeesRate = penaltyFeesRate;
+		pools[poolId].host = msg.sender;
+		pools[poolId].token = token;
+		pools[poolId].poolName = poolName;
+		pools[poolId].amountToDeposit = amountToDeposit;
+		if (cohost != address(0)) {
+			pools[poolId].cohost = cohost;
+		}
+		if (maxParticipants == 0) {
+			pools[poolId].maxParticipants = type(uint32).max;
+		} else {
+			pools[poolId].maxParticipants = maxParticipants;
+		}
+		emit PoolCreated(poolId, msg.sender, pools[poolId]);
 
 		createdPools[msg.sender].push(poolId);
 		latestPoolId++;
@@ -100,12 +132,22 @@ contract Pool is Owned {
 		PoolDetails memory pool = pools[poolId];
 		require(pool.status == POOLSTATUS.DEPOSIT_ENABLED, "Deposit not enabled");
 		require(block.timestamp <= pool.timeStart, "Deposit not allowed");
+		require(amount == pool.amountToDeposit, "Deposit amount not correct");
 
 		// Transfer tokens from user to pool
-		pool.token.transferFrom(msg.sender, address(this), amount);
+		bool success = pool.token.transferFrom(msg.sender, address(this), amount);
+		require(success, "Transfer failed");
 
+		// Update pool details
+		Participant memory participant;
+		participant.deposit = amount;
+		participant.participant = msg.sender;
+		pools[poolId].participants.push(participant);
+		pools[poolId].numParticipantsJoined++;
 		pools[poolId].totalDeposits += amount;
 		pools[poolId].balance += amount;
+
+		emit Deposit(poolId, msg.sender, amount);
 	}
 
 	// ----------------------------------------------------------------------------
